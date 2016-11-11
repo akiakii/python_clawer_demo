2c8872089c755bc5fc0eccc07bbaa288f91598d3
akiakii <651369070@qq.com>
2016-11-12 00:59:16 +0800

    python3.5_clawer on window

---
+++ python-clawer/clawer_3.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+def getchild(url):
+    try:
+        html=urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html, "html.parser").find("table",{"id":"giftList"}).children
+    except AttributeError as e:
+        return None
+    return bsObj
+bsObj =getchild("http://www.pythonscraping.com/pages/page3.html")
+for child in bsObj:
+    print(child)
